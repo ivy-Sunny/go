c66630e09496f54c12c3d8a91e4bf733b9f91c43
Sunniva <thelife_dream@icloud.com>
2021-12-22 17:27:15 +0800

    2021/12/22

--- test/waitgroup.go
+++ test/waitgroup.go
@@ -1,8 +1,15 @@
 package test
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+)
+
+var wp sync.WaitGroup
 
 func showMSG(i int) {
+	// defer wp.Add(-1) 等价于 defer wp.Wait()
+	defer wp.Wait()
 	fmt.Printf("i: %v\n", i)
 }
 
@@ -10,8 +17,9 @@ func MainWaitgroup() {
 	for i := 0; i < 10; i++ {
 		//启动一个协程来执行
 		go showMSG(i)
+		wp.Add(1)
 	}
-
+	wp.Wait()
 	//主协程
 	fmt.Println("end...")
 }
